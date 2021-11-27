77dcbe5c6ea405f8b923e8038d632414a008d4f7
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2021-11-27 11:12:02 -0500

    finish subscribing to store 'manually'

--- src/components/Bugs.js
+++ src/components/Bugs.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext, useState } from 'react';
 import StoreContext from '../contexts/storeContext';
+import { loadBugs }from '../store/bugs';
 
 function Bugs() {
   const store = useContext(StoreContext);
@@ -13,11 +14,15 @@ function Bugs() {
         setBugs(bugsInStore);
     });
 
+    store.dispatch(loadBugs());
+
     return unsubscribe;
   }, [store]);
 
   return ( 
-    <div>Bugs</div>
+    <ul>{bugs.map(bug => 
+      <li key={bugs.id}>{bug.description}</li>
+    )}</ul>
    );
 }
 
